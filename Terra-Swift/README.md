6b671efdd01d432c61531ea5d7d28c9e287429fd
TheArchitect123 <dan.developer789@gmail.com>
2022-09-05 23:29:32 +1000

    Initial Commit

---
+++ Terra-Swift/README.md
@@ -0,0 +1,3 @@
+# Terra-Swift
+
+A description of this package.
---
+++ Terra-Swift/Tests/Terra-SwiftTests/Terra_SwiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Terra_Swift
+
+final class Terra_SwiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Terra_Swift().text, "Hello, World!")
+    }
+}
